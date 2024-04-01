d735aadff7919bcb620b43f413992043cb6b6c90
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-04-01 19:44:29 +0530

    connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 const app = express();
 const PORT = 3000 ;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log(`MongoDB is connected successfully`)})
+.catch((err)=>{console.log(`Error in connecting to MongoDB: ${err}`)})
+
 
 app.get('/',(req,res)=>{
     res.send(`This is a Home Page.`)
